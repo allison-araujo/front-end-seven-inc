84c35d64065996d7f7322382cd26ecd3610d1c69
allisonvinicius <allisonvinicius63@gmail.com>
2022-08-31 22:29:10 -0400

    create setupe initial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import EditUser from "./component/Pages/EditUser";
+import NavBar from "./components/Navbar";
+import AddUser from "./Pages/AddUser/AddUser";
+import NotFound from "./Pages/Notfound";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<NotFound />} />
+        <Route path="/all" element={<AllUsers />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
