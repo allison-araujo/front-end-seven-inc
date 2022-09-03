33330ddae7c97b1792bee46f651a8d6f70765b1d
allisonvinicius <allisonvinicius63@gmail.com>
2022-09-03 00:08:03 -0400

    feat: styleguide textFiled

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,17 @@
+import { TextField } from "@mui/material";
+
+import { FormsValues } from "../../ts/types";
+
+const InputField = (values: FormsValues) => {
+  return (
+    <TextField
+      variant="outlined"
+      label={values.label}
+      name={values.name}
+      value={values.value}
+      onChange={values.onChange}
+    />
+  );
+};
+
+export default InputField;
