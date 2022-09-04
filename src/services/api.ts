be03ea40bafc825bc6d2d2d6120250d6da8b659e
allisonvinicius <allisonvinicius63@gmail.com>
2022-09-04 19:32:05 -0400

    refactory: refactory methods endpoinst

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,36 @@
 import axios from "axios";
-
 import http from "../https/http";
+
 import { Employee, IdType } from "../ts/types";
 
 export const findEmplooyers = async () => {
   try {
-    return await axios.get(`${http}`);
+    const json = await axios.get(`${http}`);
+
+    return json;
   } catch (error) {
-    console.log("erro find employers ", error);
+    console.log("Não existe funcionários cadastrados ", error);
   }
 };
 
-export const findEmplooyersDetails = async (id: IdType) => {
+export const findEmplooyersDetails = async (id: any) => {
   try {
-    return await axios.get(`/${http}/employee/${id}`);
+    return await axios.get(`/${http}/emplooye/${id}`);
   } catch (error) {
     console.log("erro find employers ", error);
   }
 };
 
 export const createEmployer = async (emplooyer: Employee) => {
-  return await axios.post(`${http}/employee`, emplooyer);
+  const json = await axios.post(`${http}/exmployee`, emplooyer);
+
+  return json;
 };
 
 export const deleteEmplooyer = async (id: IdType) => {
-  return await axios.delete(`/${http}/employee/${id}`);
+  return await axios.delete(`${http}/${id}`);
 };
 
 export const editEmplooyer = async (id: IdType, emplooyer: Employee) => {
-  return await axios.put(`/${http}/employee/${id}`, emplooyer);
+  return await axios.put(`/${http}/${id}`, emplooyer);
 };
