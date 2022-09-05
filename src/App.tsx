a8be4aead0d925e6a2a6b0beec2764dd09386ad5
allisonvinicius <allisonvinicius63@gmail.com>
2022-09-04 21:53:27 -0400

    feat: include route edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Details from "./Pages/Details/Details";
+import EditFormEmployee from "./Pages/EditEmplooyer/EditEmplooyer";
 import Employee from "./Pages/Employeer/Employee";
 import ListEmployee from "./Pages/ListEmployee/ListEmployee";
 
@@ -8,11 +9,10 @@ function App() {
   return (
     <BrowserRouter>
       <Layout>
-        {/* <Sidebar /> */}
-
         <Routes>
           <Route path="/" element={<Employee />} />
           <Route path="/employee" element={<ListEmployee />} />
+          <Route path="/employee/:id" element={<EditFormEmployee />} />
           <Route path="/employee/:id" element={<Details />} />
         </Routes>
       </Layout>
